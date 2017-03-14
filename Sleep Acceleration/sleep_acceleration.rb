1b67de0433add50da840164d41c66b51f77ef024
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-03-14 12:16:35 -0400

    Modularized ruby sleep

--- Sleep Acceleration/sleep_acceleration.rb
+++ Sleep Acceleration/sleep_acceleration.rb
@@ -5,22 +5,25 @@
 
 require 'socket'
 
-ntp_msg = (["00011011"] + Array.new(47,1)).pack("B8 C47")
-sock = UDPSocket.new;sock.connect("us.pool.ntp.org", 123)
-sock.print ntp_msg;sock.flush;
-data,_ = sock.recvfrom(960)
-sock.close
+def getNTPTime()
 
-firstTime = Time.at(data.unpack("B319 B32 B32")[1].to_i(2) - 2208988800);
+	ntp_msg = (["00011011"] + Array.new(47,1)).pack("B8 C47")
+	sock = UDPSocket.new;sock.connect("us.pool.ntp.org", 123)
+	sock.print ntp_msg;sock.flush;
+	data,_ = sock.recvfrom(960)
+	sock.close
+
+	return Time.at(data.unpack("B319 B32 B32")[1].to_i(2) - 2208988800);
+
+end
+
+firstTime = getNTPTime()
 puts "NTP time before sleeping: #{firstTime}"
-puts "Attempting to sleep for #{ARGV[0].to_i} seconds..."
 
+puts "Attempting to sleep for #{ARGV[0].to_i} seconds..."
 sleep(ARGV[0].to_i)
 
-sock = UDPSocket.new;sock.connect("us.pool.ntp.org", 123)
-sock.print ntp_msg;sock.flush
-data,_ = sock.recvfrom(960);
-secondTime = Time.at(data.unpack("B319 B32 B32")[1].to_i(2) - 2208988800)
+secondTime = getNTPTime()
 puts "NTP time after sleeping: #{secondTime}"
 
 difference = secondTime - firstTime
