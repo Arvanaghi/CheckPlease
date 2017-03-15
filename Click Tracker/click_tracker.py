11b921575e135086f898ec86729f06089d058c6e
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-03-15 19:03:01 -0400

    Click Tracker folder, python implementation

---
+++ Click Tracker/click_tracker.py
@@ -0,0 +1,24 @@
+#
+#   Python mouse click tracker
+#   Module written by Brandon Arvanaghi
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+import win32api
+import sys
+
+current_state_left_click = win32api.GetKeyState(1)
+current_state_right_click = win32api.GetKeyState(2)
+count = 0
+
+while count < int(sys.argv[1]):
+	new_state_left_click = win32api.GetAsyncKeyState(1)
+	new_state_right_click = win32api.GetAsyncKeyState(2)
+
+	if new_state_left_click % 2 == 1:
+		count += 1
+	if new_state_right_click % 2 == 1:
+		count += 1
+
+print("Proceed!")
