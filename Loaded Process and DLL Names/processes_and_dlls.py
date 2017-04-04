e39d3accf58a7e3690ce205185e998e69c6491c5
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-04-04 09:34:59 -0400

    New folder name

---
+++ Loaded Process and DLL Names/processes_and_dlls.py
@@ -0,0 +1,24 @@
+#
+#   Windows Running Process and Loaded DLL Name Checker, Python
+#   Module written by Brandon Arvanaghi
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+import win32pdh
+
+EvidenceOfSandbox = []
+sandboxProcesses = "vmsrvc", "tcpview", "wireshark", "visual basic", "fiddler", "vmware", "vbox", "process explorer", "autoit", "vboxtray", "vmtools", "vmrawdsk", "vmusbmouse", "vmvss", "vmscsi", "vmxnet", "vmx_svga", "vmmemctl", "df5serv", "vboxservice", "vmhgfs"
+
+_, runningProcesses = win32pdh.EnumObjectItems(None,None,'process', win32pdh.PERF_DETAIL_WIZARD)
+
+for process in runningProcesses:
+    for sandboxProcess in sandboxProcesses:
+        if sandboxProcess in str(process):
+            EvidenceOfSandbox.append(process)
+            break
+
+if not EvidenceOfSandbox:
+    print("Proceed!")
+else:
+    print(EvidenceOfSandbox)
