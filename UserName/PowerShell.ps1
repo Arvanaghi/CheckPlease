5b685a98754ea6cae40e76a6c3a42947fe98bb17
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-04-06 09:25:31 -0400

    Restructuring..

--- UserName/PowerShell.ps1
+++
@@ -1,6 +0,0 @@
-$username = $env:username.ToLower()
-
-if($username -eq "testusername") {
-    $message = "Put your code here"
-    $message
-}
--- UserName/python.py
+++
@@ -1,6 +0,0 @@
-import getpass
-targeted_username = input("\n [>] What username should we validate is being used?")
-user_name = getpass.getuser()
-if targeted_username.lower() in user_name:
-    # Run the rest of your code now
-    pass
