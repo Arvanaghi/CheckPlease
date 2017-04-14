7aeaf1cc93eccfca1c0a06e75ece0baa946ecc7f
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-04-14 11:35:35 -0400

    MAC address check, PowerShell

---
+++ MAC Address/mac address.ps1
@@ -0,0 +1,25 @@
+#
+#   MAC Address Checker, PowerShell
+#   Module written by Brandon Arvanaghi
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+$EvidenceOfSandbox = New-Object System.Collections.ArrayList
+
+$BadMACAddresses = '00:0C:29', '00:1C:14', '00:50:56', '00:05:69', '08:00:27'
+$MACAddresses = Get-WmiObject Win32_NetworkAdapterConfiguration | Select -ExpandProperty MACAddress
+
+ForEach ($MACAddress in $MACAddresses) {
+	ForEach ($BadMACAddress in $BadMACAddresses) {
+		if ($MACAddress | Select-String $BadMACAddress) {
+			[void]$EvidenceOfSandbox.Add($MACAddress)
+		}
+	}
+}
+
+if ($EvidenceOfSandbox.count -eq 0) {
+	Write-Output "Proceed!"
+} else {
+	$EvidenceOfSandbox
+}
