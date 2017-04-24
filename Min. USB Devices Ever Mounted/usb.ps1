01a89fe62fb5fef03a5c2bcb1dc9c46ea564ec5c
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-04-24 12:42:46 -0400

    USB device history, PS

---
+++ Min. USB Devices Ever Mounted/usb.ps1
@@ -0,0 +1,20 @@
+#
+#   Minimum number of USB devices ever mounted, PowerShell
+#   Module written by Brandon Arvanaghi
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+if ($($args[0]).count -eq 0) {
+	$MinimumUSBHistory = 1
+} else {
+	$MinimumUSBHistory = $($args[0])
+}
+
+$usbCount = (Get-ChildItem HKLM:\SYSTEM\ControlSet001\Enum\USBSTOR).Name.Count
+
+if ($usbCount -ge $MinimumUSBHistory) {
+	Write-Output "Proceed!"
+} else {
+	Write-Output "Number of USB Devices Ever Mounted: $usbCount"
+}
