2263c40247b9df0c1017dbf5108afac6aeeb9034
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-05-29 15:49:45 -0400

    Min processes, C

---
+++ Min. Running Processes/minimum_num_running_processes.c
@@ -0,0 +1,42 @@
+/*
+	Minimum Number of Running Processes, C
+	Ensures at least N processes running on the system (defaults to 50)
+	Module written by Brandon Arvanaghi
+	Website: arvanaghi.com
+	Twitter: @arvanaghi
+*/
+
+#include <windows.h>
+#include <stdio.h>
+#include <psapi.h>
+
+int main(int argc, char **argv[]) {
+	int minimumNumberOfProcesses = 50;
+
+	if (argc > 1) {
+		minimumNumberOfProcesses = atoi(argv[1]);
+	}
+
+	DWORD loadedProcesses[1024];
+	DWORD cbNeeded;
+	DWORD numProcessesFound;
+
+	// Get all PIDs
+	if (!EnumProcesses(loadedProcesses, sizeof(loadedProcesses), &cbNeeded)) {
+		printf("[---] Could not get all PIDs, exiting.\n");
+		getchar();
+		exit(-1);
+	}
+
+	// Calculate how many PIDs returned
+	numProcessesFound = cbNeeded / sizeof(DWORD);
+
+	if (numProcessesFound > minimumNumberOfProcesses) {
+		printf("Proceed!\n");
+	} else {
+		printf("%d processes running on this host.\n", numProcessesFound);
+	}
+
+	getchar();
+	return 0;
+}
