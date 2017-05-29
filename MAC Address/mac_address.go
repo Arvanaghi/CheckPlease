f4d9f6329033100b878588ce87ba5aae718ab600
Brandon Arvanaghi <brandonarvanaghi@gmail.com>
2017-05-29 09:22:55 -0400

    Update mac_address.go

--- MAC Address/mac_address.go
+++ MAC Address/mac_address.go
@@ -15,23 +15,23 @@ import (
 
 func main() {
 
-    EvidenceOfSandbox := make([]net.HardwareAddr, 0)
+  EvidenceOfSandbox := make([]net.HardwareAddr, 0)
 
-    badMacAddresses := [...]string{`00:0C:29`, `00:1C:14`, `00:50:56`, `00:05:69`, `08:00:27`}
+  badMacAddresses := [...]string{`00:0C:29`, `00:1C:14`, `00:50:56`, `00:05:69`, `08:00:27`}
 
   NICs, _ := net.Interfaces()
   for _, NIC := range NICs {
     for _, badMacAddress := range badMacAddresses {
-        if strings.Contains(strings.ToLower(NIC.HardwareAddr.String()), strings.ToLower(badMacAddress)) {
-          EvidenceOfSandbox = append(EvidenceOfSandbox, NIC.HardwareAddr)
-        }
+      if strings.Contains(strings.ToLower(NIC.HardwareAddr.String()), strings.ToLower(badMacAddress)) {
+        EvidenceOfSandbox = append(EvidenceOfSandbox, NIC.HardwareAddr)
+      }
     }   
   }
 
-    if len(EvidenceOfSandbox) == 0 {
-        fmt.Println("Proceed!")
-    } else {
-        fmt.Println(EvidenceOfSandbox)
-    }
+  if len(EvidenceOfSandbox) == 0 {
+    fmt.Println("Proceed!")
+  } else {
+    fmt.Println(EvidenceOfSandbox)
+  }
 
 }
