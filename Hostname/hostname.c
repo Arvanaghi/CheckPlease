18cd21e2c418c8f73431e1175bcb84b3b002383f
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-05-29 14:31:10 -0400

    Hostname.cs

---
+++ Hostname/hostname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int wmain(int agrc, wchar_t **argv[]) {
+
+	WCHAR* computerName[3267];
+	DWORD charCount[3267];
+
+	if (!GetComputerNameW(&computerName, &charCount)) {
+		printf("Could not read computer name, exiting.\n");
+		getchar();
+		exit(-1);
+	}
+
+	if (!wcsicmp(computerName, argv[1])) {
+		printf("Proceed!\n");
+	} else {
+		wprintf(L"Actual hostname: %s", computerName);
+	}
+
+	getchar();
+	return 0;
+}
