d1694830cce2c9e4e4c232ebfdf850ec33cf5a8b
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-10 11:01:54 -0400

    Debug prevention C

--- Anti-Debug/Debugging Prevention/prevent_debugging.c
+++ Anti-Debug/Debugging Prevention/prevent_debugging.c
@@ -1,41 +1,25 @@
 /*
-    Prevents process from being debugged, C
-    Module written by Brandon Arvanaghi 
-    Website: arvanaghi.com 
-    Twitter: @arvanaghi
+	Prevents a debugger from attaching to this process after it has been loaded, C
+	Module written by Brandon Arvanaghi
+	Website: arvanaghi.com
+	Twitter: @arvanaghi
 */
 
 #include "Windows.h"
 #include "Winternl.h"
 #include "stdio.h"
 
-typedef NTSTATUS (WINAPI *pNtQueryInformationProcess) (
-	HANDLE ProcessHandle,
-	PROCESSINFOCLASS ProcessInformationClass,
-	PVOID ProcessInformation,
-	ULONG ProcessInformationLength,
-	PULONG ReturnLength
-);
+int main(int argc, char **argv[]) {
 
-int main(int argc, char *argv[]) {
-
-	PROCESS_BASIC_INFORMATION ProcessInfo;
-	// Pointed to Process Environment Block structure
-	PPEB pPEB;
-	
-	// Load in ntdll, get offset of function NtQueryInformationProcess function
-	pNtQueryInformationProcess NtQueryInformationProcess = (pNtQueryInformationProcess)GetProcAddress(LoadLibrary(L"ntdll.dll"), "NtQueryInformationProcess");
-	// Use function pointer to call NtQueryInformationProcess
-	NtQueryInformationProcess(GetCurrentProcess(), ProcessBasicInformation, &ProcessInfo, sizeof(PROCESS_BASIC_INFORMATION), NULL);
-	// Store the base address of the PEB from the ProcessInformation struct
-	pPEB = (PPEB)ProcessInfo.PebBaseAddress;
+	PPEB pPEB = (PPEB)__readfsdword(0x30);
 
 	// Pretend process is already being debugged by setting PEB's BeingDebugged byte to 1
 	// Only one debugger can attach to a process at a time.
-	pPEB->BeingDebugged = 0x11;
+	pPEB->BeingDebugged = 1;
 
 	printf("The Process Environment Block's \"BeingDebugged\" field is set. Proceed!")
 
 	getchar();
 	return 0;
+
 }
