b0f4d4674c8048435916b15005a89938879251db
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-17 15:42:47 -0400

    User prompt, Go, PS

---
+++ User Prompt/user_prompt.go
@@ -0,0 +1,37 @@
+/*
+    Prompts user with dialog box and waits for response before executing, Go
+    Module written by Brandon Arvanaghi 
+    Website: arvanaghi.com 
+    Twitter: @arvanaghi
+*/
+
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+	"os"
+)
+
+func main() {
+
+	var dialogBoxTitle = "CheckPlease by @arvanaghi and @ChrisTruncer";
+	var dialogBoxMessage = "This is a sample dialog box to ensure user acitivity!"
+
+  if (len(os.Args) == 3) {
+    dialogBoxTitle = os.Args[1]
+    dialogBoxMessage = os.Args[2]
+  }
+
+	var user32dll = syscall.NewLazyDLL("user32.dll")
+	var messageBoxW = user32dll.NewProc("MessageBoxW")
+
+	messageBoxW.Call(0,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(dialogBoxMessage))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(dialogBoxTitle))),
+		0)
+
+	fmt.Println("Now that the user has clicked \"OK\" or closed the dialog box, we will proceed with malware execution!")
+
+}
---
+++ User Prompt/user_prompt.ps1
@@ -0,0 +1,19 @@
+#
+#   Prompts user with dialog box and waits for response before executing, PowerShell
+#   Module written by Brandon Arvanaghi
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+$dialogBoxTitle = "CheckPlease by @arvanaghi and @ChrisTruncer"
+$dialogBoxMessage = "This is a sample dialog box to ensure user acitivity!"
+
+if ($Args.count -eq 2) {
+	$dialogBoxTitle = $($args[0])
+	$dialogBoxMessage = $($args[1])
+}
+
+$messageBox = New-Object -COMObject WScript.Shell
+[void]$messageBox.Popup($dialogBoxMessage,0,$dialogBoxTitle,0)
+
+Write-Output "Now that the user has clicked `"OK`" or closed the dialog box, we will proceed with malware execution!"
