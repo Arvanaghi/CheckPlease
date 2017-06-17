08cf8d952f443a022f68f020251ac8fde5693d79
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-17 14:13:15 -0400

    user prompt, Python

---
+++ User Prompt/user_prompt.py
@@ -0,0 +1,21 @@
+#
+#   Prompts user with dialog box and waits for response before executing, Python
+#   Module written by Brandon Arvanaghi
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+import ctypes
+import sys
+
+dialogBoxTitle = "CheckPlease by @arvanaghi and @ChrisTruncer";
+dialogBoxMessage = "This is a sample dialog box to ensure user acitivity!"
+
+if len(sys.argv) == 3:
+	dialogBoxTitle = sys.argv[1]
+	dialogBoxMessage = sys.argv[2]
+
+MessageBox = ctypes.windll.user32.MessageBoxA
+MessageBox(None, dialogBoxMessage, dialogBoxTitle, 0)
+
+print("Now that the user has clicked \"OK\" or closed the dialog box, we will proceed with malware execution!")
