5c84c7dd25e0f01fdd57f303823eef7a11b9fb23
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-17 15:53:07 -0400

    User prompt, Perl

---
+++ User Prompt/user_prompt.pl
@@ -0,0 +1,18 @@
+#
+#   Prompts user with dialog box and waits for response before executing, Perl
+#   Module written by Brandon Arvanaghi 
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+use Win32;
+
+my $dialogBoxTitle = "CheckPlease by \@arvanaghi and \@ChrisTruncer";
+my $dialogBoxMessage = "This is a sample dialog box to ensure user acitivity!";
+
+if (@ARGV == 2) {
+	$dialogBoxTitle = $ARGV[0];
+	$dialogBoxMessage = $ARGV[1];
+}
+
+Win32::MsgBox($dialogBoxMessage, 0, $dialogBoxTitle);
