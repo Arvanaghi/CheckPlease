98288be3048f4f7e92f5fe906b22011d5199fc47
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-18 22:28:25 -0400

    Cursor position woriding, Ruby

--- Cursor Position/cursor_position.rb
+++ Cursor Position/cursor_position.rb
@@ -9,26 +9,26 @@ require "Win32API"
 
 secs = 1200
 if ARGV.length == 1
-  secs = ARGV[0].to_i
+	secs = ARGV[0].to_i
 end
 
-def getCoords()
-  getCursorPos = Win32API.new("user32", "GetCursorPos", ['P'], 'L')
-  coordinates = "\0" * 8
-  getCursorPos.call(coordinates)
-  return coordinates.unpack('LL')
+def getPoint()
+	getCursorPos = Win32API.new("user32", "GetCursorPos", ['P'], 'L')
+	pt = "\0" * 8
+	getCursorPos.call(pt)
+	return pt.unpack('LL')
 end
 
-firstCoords = getCoords()
-puts "x: " + firstCoords[0].to_s + ", y: " + firstCoords[1].to_s
+firstPoint = getPoint()
+puts "x: " + firstPoint[0].to_s + ", y: " + firstPoint[1].to_s
 
 sleep(secs)
 
-secondCoords = getCoords()
-puts "x: " + secondCoords[0].to_s + ", y: " + secondCoords[1].to_s
+secondPoint = getPoint()
+puts "x: " + secondPoint[0].to_s + ", y: " + secondPoint[1].to_s
 
-if firstCoords[0] - secondCoords[0] && firstCoords[1] - secondCoords[1] == 0
-  puts "The cursor has not moved in the last " + secs.to_s + " seconds. Do not proceed."
+if firstPoint[0] - secondPoint[0] && firstPoint[1] - secondPoint[1] == 0
+	puts "The cursor has not moved in the last " + secs.to_s + " seconds. Do not proceed."
 else
-  puts "The cursor is not in the same position as it was " + secs.to_s + " seconds ago. Proceed!"
+	puts "The cursor is not in the same position as it was " + secs.to_s + " seconds ago. Proceed!"
 end
