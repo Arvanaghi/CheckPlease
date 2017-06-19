963bd4c43c65a543ebf4c9883f5773395fcc907e
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-19 19:21:55 -0400

    Cursor position, Perl

---
+++ Cursor Position/cursor_position.pl
@@ -0,0 +1,39 @@
+#
+#   Checks if cursor is in same position after sleeping N seconds (default: 20 min), Perl
+#   Module written by Brandon Arvanaghi 
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+use Win32::API;
+
+my $secs = 1;
+if (@ARGV == 1) {
+	$secs = $ARGV[0];
+}
+
+Win32::API::Struct->typedef( POINT => qw{
+    LONG x; 
+    LONG y; 
+});
+
+Win32::API->Import('user32', 'BOOL GetCursorPos(LPPOINT lpPoint)');
+
+my $point = Win32::API::Struct->new('POINT');
+$point->align('auto');
+$point->{'x'} = 0; $point->{'y'} = 0;
+GetCursorPos($point);
+print "x: $point->{x}, y: $point->{y}\n";
+
+sleep($secs);
+
+my $point2 = Win32::API::Struct->new('POINT');
+$point2->{'x'} = 0; $point2->{'y'} = 0;
+GetCursorPos($point2);
+print "x: $point2->{x}, y: $point2->{y}\n";
+
+if ($point->{x} - $point2->{x} == 0 && $point->{y} - $point2->{y} == 0) {
+	print "The cursor has not moved in the last $secs seconds. Do not proceed.\n"
+} else {
+	print "The cursor is not in the same position as it was $secs seconds ago. Proceed!\n"
+}
