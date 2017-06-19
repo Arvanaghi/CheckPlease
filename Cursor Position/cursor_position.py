015301c6147c57902dbda3eb7028b3f3fe846715
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-18 21:47:39 -0400

    Cursor pos, Py, updated

--- Cursor Position/cursor_position.py
+++ Cursor Position/cursor_position.py
@@ -5,31 +5,23 @@
 #   Twitter: @arvanaghi
 #
 
-from ctypes import Structure, windll, byref, c_ulong
 from time import sleep
+import win32api
 import sys
 
 secs = 1200;
 if len(sys.argv) == 2:
-  secs = float(sys.argv[1])
+	secs = float(sys.argv[1])
 
-class tagPOINT(Structure):
-    _fields_ = [("x", c_ulong), ("y", c_ulong)]
-
-def getCoords():
-  coord = tagPOINT()
-  windll.user32.GetCursorPos(byref(coord))
-  return coord
-
-firstCoord = getCoords()
-print("x: " + str(firstCoord.x) + ", y: " + str(firstCoord.y))
+x, y = win32api.GetCursorPos()
+print("x: " + str(x) + ", y: " + str(y))
 
 sleep(secs)
 
-secondCoord = getCoords()
-print("x: " + str(secondCoord.x) + ", y: " + str(secondCoord.y))
+x2, y2 = win32api.GetCursorPos()
+print("x: " + str(x2) + ", y: " + str(y2))
 
-if firstCoord.x - secondCoord.x == 0 and firstCoord.y - secondCoord.y == 0:
-  print("The cursor has not moved in the last {} seconds. Do not proceed.".format(secs))
+if x - x2 == 0 and y - y2 == 0:
+	print("The cursor has not moved in the last {} seconds. Do not proceed.".format(secs))
 else:
-  print("The cursor is not in the same position as it was {} seconds ago. Proceed!".format(secs))
+	print("The cursor is not in the same position as it was {} seconds ago. Proceed!".format(secs))
