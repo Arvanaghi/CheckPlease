9e5c33d949d086a2c55916a7691e12f05cc76691
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-24 14:30:22 -0400

    Parent process, PS

---
+++ Parent Processs/parent_process.ps1
@@ -0,0 +1,20 @@
+#
+#   Ensures the name of the parent process that spawned the payload is as expected, PowerShell
+#   Module written by Brandon Arvanaghi
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+$expectedParentProc = "firefox"
+if ($Args.count -eq 1) {
+	$expectedParentProc = $($args[0])
+}
+
+$pPid = (Gwmi Win32_Process | ? ProcessID -eq  $pid).ParentProcessID
+$actualParentProc = (Get-Process -ID $pPid).ProcessName
+
+if ((Get-Process -ID $pPid).ProcessName -eq $expectedParentProc) {
+	Write-Output "As expected, the parent process for this process is `"$actualParentProc`". Proceed!"	
+} else {
+	Write-Output "The parent process for this process is `"$actualParentProc`", not `"$expectedParentProc`" as you expected. Do not proceed."
+}
