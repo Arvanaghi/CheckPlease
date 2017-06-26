b95679ccf74a51acfc6d61754395f599b3c9a2dd
Brandon Arvanaghi <brandonarvanaghi@gmail.com>
2017-06-26 07:27:42 -0400

    Thanks Chris, error in PS browser

--- Min. Web Browsers/minimum_num_browsers.ps1
+++ Min. Web Browsers/minimum_num_browsers.ps1
@@ -10,13 +10,13 @@ $browserCount = 0
 $browserKeys = 'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe', 'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\iexplore.exe', 'SOFTWARE\Mozilla'
 
 ForEach ($browserKey in $browserKeys) {
-	if (Test-Path ("HKLM:\" + $HKLM_Key)) {
-		++$browserCount;
-	}
+  if (Test-Path ("HKLM:\$browserKey")) {
+    ++$browserCount;
+  }
 }
 
 if ($browserCount -ge 2) {
-	Write-Output "Proceed!"
+  Write-Output "There are at least 2 web browsers present on the system. Proceed!"
 } else {
-	Write-Output "Number of Browsers: $browserCount"
+  Write-Output "Less than 2 web browsers exist on the system, do not proceed (found: $browserCount)."
 }
