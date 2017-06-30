9b4f8ecc58149d1d8d86460374d26abc02f73bcd
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-30 10:08:36 -0400

    Process Names, Perl (updated)

--- Process Names/check_all_process_names.pl
+++ Process Names/check_all_process_names.pl
@@ -1,33 +1,23 @@
 #
 #   Checks all loaded process names, Perl
 #   Module written by Brandon Arvanaghi 
-#   Logic of extracting pids and process names from `tasklist` taken from https://stackoverflow.com/questions/16191112/windows-running-processes-list-perl
 #   Website: arvanaghi.com 
 #   Twitter: @arvanaghi
 #
 
+use Win32::OLE;
+
 my @EvidenceOfSandbox;
 my @sandboxProcesses = (q{vmsrvc}, q{tcpview}, q{wireshark}, q{visual basic}, q{fiddler}, q{vmware}, q{vbox}, q{process explorer}, q{autoit}, q{vboxtray}, q{vmtools}, q{vmrawdsk}, q{vmusbmouse}, q{vmvss}, q{vmscsi}, q{vmxnet}, q{vmx_svga}, q{vmmemctl}, q{df5serv}, q{vboxservice}, q{vmhgfs});
 
-my @procs = `tasklist`;
+my $runningProcesses = Win32::OLE->GetObject("winmgmts:\\\\localhost\\root\\CIMV2")->ExecQuery("SELECT * FROM Win32_Process") or die;
 
-my $pid_pos;
-if ($procs[2] =~ /^=+/) { $pid_pos = $+[0]+1; } else { die; }
-
-my %pids;
-for (@procs[3 .. $#procs]) {
-	my $name = substr $_,0,$pid_pos;
-	$name =~s/^\s+|\s+$//g;
-
-	if (substr($_,$pid_pos) =~ /^\s*(\d+)/) {
-		$pids{$1} = $name;	
-	}
-}
-
-while(my($pid, $processName) = each %pids) {
+foreach my $process (in $runningProcesses) {
 	foreach $sandboxProcess (@sandboxProcesses) {
-		if (index($processName, $sandboxProcess) != -1) {
-			push(@EvidenceOfSandbox, $processName);
+		if (index($process->{Name}, $sandboxProcess) != -1) {
+			if (!(grep $_ eq $process->{Name}, @EvidenceOfSandbox)) {
+				push(@EvidenceOfSandbox, $process->{Name});
+			}
 		} 
 	}
 }
