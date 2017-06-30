4e33842fd765331a0ad141e82c908ba7c6c3999e
Brandon Arvanaghi <brandon.arvanaghi@mandiant.com>
2017-06-30 13:58:15 -0400

    Date trigger, Ruby

---
+++ Date Trigger/date_trigger.rb
@@ -0,0 +1,19 @@
+#
+#   Waits until a user-defined date to execute, Ruby
+#   Module written by Brandon Arvanaghi 
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+require 'date'
+
+if ARGV.length != 1
+	puts "You must provide a date in format mm/dd/yyyy."
+	exit(1)
+end
+
+while (Time.now.to_date > Date.strptime(ARGV[0], "%m/%d/%Y").to_date)
+	sleep(86340)
+end
+
+puts "Now that today is " + Time.now.to_date.strftime("%m/%d/%Y").to_s + ", we may proceed with malware execution!"
