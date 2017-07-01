34f1c049eae10ee9dddce6c11d876f72990680c2
Brandon Arvanaghi <brandonarvanaghi@gmail.com>
2017-07-01 19:51:47 -0400

    Current file name, Python

---
+++ Current File Name/current_file_name.py
@@ -0,0 +1,22 @@
+#
+#   Ensures the current file name is as expected, Python
+#   Module written by Brandon Arvanaghi 
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+import os
+import sys
+
+expectedFileName = ""
+
+if len(sys.argv) != 2:
+  print("You must provide a file name to check for.")
+  exit(1)
+else:
+  expectedFileName = sys.argv[1]
+
+if os.path.basename(__file__) == expectedFileName:
+  print("The file name is {} as expected. Proceed!".format(expectedFileName))
+else:
+  print("The file name {0}, and not {1} as expected. Do not proceed.".format(os.path.basename(__file__), expectedFileName))
