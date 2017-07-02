9c0906d68c397652b2bc68fa55f64fa4c6f2fa5a
Brandon Arvanaghi <brandonarvanaghi@gmail.com>
2017-07-02 14:48:38 -0400

    Registry size, PowerShell, Python

---
+++ Registry Size/registry_size.ps1
@@ -0,0 +1,20 @@
+#
+#   Minimum Registry size checker (default: 55 MB), PowerShell
+#   Module written by Brandon Arvanaghi
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+if ($Args.count -eq 0) {
+  $minRegSizeMB = 55
+} else {
+  $minRegSizeMB = $($args[0])
+}
+
+$regSize = GWMI -Class Win32_Registry | Select-Object -Expand CurrentSize
+
+if ($regSize -gt $minRegSizeMB) {
+  Write-Output "The size of the Registry on this host is $regSize MB, which is greater than the minimum you set of $minRegSizeMB MB. Proceed!"
+} else {
+  Write-Output "The size of the Registry on this host is $regSize, which is less than the minimum you set of $minRegSizeMB MB. Do not proceed."
+}
---
+++ Registry Size/registry_size.py
@@ -0,0 +1,23 @@
+#
+#   Minimum Registry size checker (default: 55 MB), Python
+#   Module written by Brandon Arvanaghi
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
+import sys
+import win32com
+from win32com.client import GetObject	
+
+minRegSizeMB = 55
+
+if len(sys.argv) > 1:
+	minRegistrySizeMB	= int(sys.argv[1])
+
+regObjects = GetObject("winmgmts:").ExecQuery("SELECT CurrentSize FROM Win32_Registry")	
+
+for regObject in regObjects:
+	if int(regObject.Properties_('CurrentSize')) > minRegistrySizeMB:
+		print("The size of the Registry on this host is {0} MB, which is greater than the minimum you set of {1} MB. Proceed!".format(regObject.Properties_('CurrentSize'), minRegistrySizeMB))
+	else:
+		print("The size of the Registry on this host is {0} MB, which is less than the minimum you set of {1} MB. Do not proceed.".format(regObject.Properties_('CurrentSize'), minRegistrySizeMB))
