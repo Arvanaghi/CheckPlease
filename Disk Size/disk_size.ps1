ea16571ebdc1d2b97d6f3255ae022ae75cc882ab
Brandon Arvanaghi <brandonarvanaghi@gmail.com>
2017-07-02 20:21:44 -0400

    GB omitted

--- Disk Size/disk_size.ps1
+++ Disk Size/disk_size.ps1
@@ -14,7 +14,7 @@ if ($Args.count -eq 0) {
 $diskSize = (GWMI -Class Win32_LogicalDisk | Measure-Object -Sum Size | Select-Object -Expand Sum) / 1073741824 
 
 if ($diskSize -gt $minDiskSizeGB) {
-  Write-Output "The disk size of this host is $diskSize, which is greater than the minimum you set of $minDiskSizeGB GB. Proceed!"
+  Write-Output "The disk size of this host is $diskSize GB, which is greater than the minimum you set of $minDiskSizeGB GB. Proceed!"
 } else {
-  Write-Output "The disk size of this host is $diskSize, which is less than the minimum you set of $minDiskSizeGB GB. Do not proceed."
+  Write-Output "The disk size of this host is $diskSize GB, which is less than the minimum you set of $minDiskSizeGB GB. Do not proceed."
 }
