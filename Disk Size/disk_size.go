db3915394c49f83f45f88898dcf8a23a185a1af7
Brandon Arvanaghi <brandonarvanaghi@gmail.com>
2017-07-02 22:41:59 -0400

    Float

--- Disk Size/disk_size.go
+++ Disk Size/disk_size.go
@@ -1,48 +1,49 @@
 /*
-    Minimum disk size checker (default: 50 GB), Go
-    Module written by Brandon Arvanaghi
-    Website: arvanaghi.com 
-    Twitter: @arvanaghi
+		Minimum disk size checker (default: 50 GB), Go
+		Module written by Brandon Arvanaghi
+		Website: arvanaghi.com 
+		Twitter: @arvanaghi
 */
 
 package main
 
 import (
-  "fmt"
-  "syscall"
-  "strconv"
-  "os"
-  "unsafe"
+	"fmt"
+	"syscall"
+	"strconv"
+	"os"
+	"unsafe"
 )
 
 func main() {
 
-  minDiskSizeGB := 50
+	minDiskSizeGB := float32(50.0)
 
-  if (len(os.Args) > 1) {
-    minDiskSizeGB, _ = strconv.Atoi(os.Args[1])
-  }
+	if (len(os.Args) > 1) {
+		val, _ := strconv.ParseFloat(os.Args[1], 32)
+		minDiskSizeGB = float32(val)
+	}
 
-  var kernel32 = syscall.NewLazyDLL("kernel32.dll")
-  var getDiskFreeSpaceEx = kernel32.NewProc("GetDiskFreeSpaceExW")
+	var kernel32 = syscall.NewLazyDLL("kernel32.dll")
+	var getDiskFreeSpaceEx = kernel32.NewProc("GetDiskFreeSpaceExW")
 
-  lpFreeBytesAvailable := int64(0)
-    lpTotalNumberOfBytes := int64(0)
-    lpTotalNumberOfFreeBytes := int64(0)
+	lpFreeBytesAvailable := int64(0)
+		lpTotalNumberOfBytes := int64(0)
+		lpTotalNumberOfFreeBytes := int64(0)
 
-  getDiskFreeSpaceEx.Call(
-    uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("C:"))),
-      uintptr(unsafe.Pointer(&lpFreeBytesAvailable)), 
-      uintptr(unsafe.Pointer(&lpTotalNumberOfBytes)),
-      uintptr(unsafe.Pointer(&lpTotalNumberOfFreeBytes)))
+	getDiskFreeSpaceEx.Call(
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("C:"))),
+		uintptr(unsafe.Pointer(&lpFreeBytesAvailable)), 
+		uintptr(unsafe.Pointer(&lpTotalNumberOfBytes)),
+		uintptr(unsafe.Pointer(&lpTotalNumberOfFreeBytes)))
 
 
-  diskSizeGB := float32(lpTotalNumberOfBytes)/1073741824
+	diskSizeGB := float32(lpTotalNumberOfBytes)/1073741824
 
-  if (diskSizeGB > float32(minDiskSizeGB)) {
-    fmt.Printf("The disk size of this host is %f GB, which is greater than the minimum you set of %d GB. Proceed!\n", diskSizeGB, minDiskSizeGB)
-  } else {
-    fmt.Printf("The disk size of this host is %f GB, which is less than the minimum you set of %d GB. Do not proceed.\n", diskSizeGB, minDiskSizeGB)
-  }
+	if (diskSizeGB > minDiskSizeGB) {
+		fmt.Printf("The disk size of this host is %f GB, which is greater than the minimum you set of %f GB. Proceed!\n", diskSizeGB, minDiskSizeGB)
+	} else {
+		fmt.Printf("The disk size of this host is %f GB, which is less than the minimum you set of %f GB. Do not proceed.\n", diskSizeGB, minDiskSizeGB)
+	}
 
 }
