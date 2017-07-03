0fce72b6218b79a78370a12dc2a9f3ceebdafbc9
Brandon Arvanaghi <brandonarvanaghi@gmail.com>
2017-07-03 13:13:17 -0400

    Windows updates.rb

--- Windows Updates/windows_updates.rb
+++ Windows Updates/windows_updates.rb
@@ -14,7 +14,6 @@ end
 
 numWindowsUpdates = (WIN32OLE.connect("winmgmts://").ExecQuery("SELECT * from win32_reliabilityRecords WHERE Sourcename = 'Microsoft-Windows-WindowsUpdateClient'")).count
 
-
 if numWindowsUpdates > minUpdates
   puts "#{numWindowsUpdates} Windows Updates have been installed on the system, which is greater than the minimum you set of #{minUpdates}. Proceed!"
 else
