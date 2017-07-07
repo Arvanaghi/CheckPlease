f6ee721c3bb6d605284dec475bd066bfdc4d1a66
Brandon Arvanaghi <brandonarvanaghi@gmail.com>
2017-07-07 13:54:28 +0100

    username, rb

--- Username/username.rb
+++ Username/username.rb
@@ -1,3 +1,12 @@
+#
+#   Checks username, Ruby
+#   Module written by Brandon Arvanaghi 
+#   Website: arvanaghi.com 
+#   Twitter: @arvanaghi
+#
+
 if ENV["USERNAME"].downcase == ARGV.join(" ").downcase
-	puts "Proceed!"
+	puts "The user account running this script is " + ENV["USERNAME"].downcase + " as expected. Proceed!"
+else
+  puts "The user account running this script is " + ENV["USERNAME"].downcase + ", not " + ARGV.join(" ").downcase + " as expected. Do not proceed."
 end
